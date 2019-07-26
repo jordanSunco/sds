c8d226a16f0044196ac90d8518f9c69da8226ee1
jordan <jordan@dawnwin.com>
2019-07-26 21:19:17 +0800

    修改删除设备逻辑，清除所有用户数据

--- sds-demo/delivery/src/main/java/com/dawnwin/stick/service/impl/StickDeviceServiceImpl.java
+++ sds-demo/delivery/src/main/java/com/dawnwin/stick/service/impl/StickDeviceServiceImpl.java
@@ -3,12 +3,11 @@ package com.dawnwin.stick.service.impl;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.dawnwin.stick.mapper.StickDeviceMapper;
-import com.dawnwin.stick.model.StickDevice;
-import com.dawnwin.stick.model.StickUserDevice;
-import com.dawnwin.stick.service.StickDeviceService;
-import com.dawnwin.stick.service.StickUserDeviceService;
+import com.dawnwin.stick.model.*;
+import com.dawnwin.stick.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Map;
@@ -19,6 +18,16 @@ public class StickDeviceServiceImpl extends ServiceImpl<StickDeviceMapper, Stick
     private StickDeviceMapper stickDeviceMapper;
     @Autowired
     private StickUserDeviceService userDeviceService;
+    @Autowired
+    private StickHeartBloodService heartBloodService;
+    @Autowired
+    private StickWarnService warnService;
+    @Autowired
+    private StickGPSService gpsService;
+    @Autowired
+    private StickFenceService fenceService;
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Override
     public StickDevice findDeviceByImei(String imei) {
@@ -61,11 +70,32 @@ public class StickDeviceServiceImpl extends ServiceImpl<StickDeviceMapper, Stick
         }
         dev.reset();
         updateById(dev);
+        StickHeartBlood hb = new StickHeartBlood();
+        hb.setDeviceId(dev.getDeviceId());
+        heartBloodService.delete(new EntityWrapper<>(hb));
+        StickWarn w = new StickWarn();
+        w.setDeviceId(dev.getDeviceId());
+        warnService.delete(new EntityWrapper<>(w));
+        StickGPS gps = new StickGPS();
+        gps.setDeviceId(dev.getDeviceId());
+        gpsService.delete(new EntityWrapper<>(gps));
+        StickFence fc = new StickFence();
+        fc.setDeviceId(dev.getDeviceId());
+        List<StickFence> fences = fenceService.selectList(new EntityWrapper<>(fc));
+        try {
+            for (StickFence fence : fences) {
+                restTemplate.delete("https://restapi.amap.com/v4/geofence/meta?key=178d7cef1209656b6d17dda618778330&gid=" + fence.getAmapGid());
+
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        fenceService.delete(new EntityWrapper<>(fc));
+
     }
 
     @Override
     public boolean updateById(StickDevice entity) {
-        super.updateById(entity);
         return stickDeviceMapper.updateById(entity)>0;
     }
 }
